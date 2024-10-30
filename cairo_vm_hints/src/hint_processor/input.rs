dec58324cffbde26f0eefc0267eab58ed0d425c3
Bartosz Nowak <bartekn337@gmail.com>
2024-10-30 11:49:22 +0100

    rust hints adjusted

--- cairo_vm_hints/src/hint_processor/input.rs
+++ cairo_vm_hints/src/hint_processor/input.rs
@@ -10,9 +10,9 @@ use cairo_vm::{
 use eth_essentials_cairo_vm_hints::utils;
 use std::collections::HashMap;
 
-pub const HINT_INPUT: &str = "ids.from_block_number_high=program_input['from_block_number_high']\nids.to_block_number_low=program_input['to_block_number_low']\nids.mmr_offset=program_input['mmr_last_len'] \nids.mmr_last_root_poseidon=program_input['mmr_last_root_poseidon']\nids.mmr_last_root_keccak.low=program_input['mmr_last_root_keccak_low']\nids.mmr_last_root_keccak.high=program_input['mmr_last_root_keccak_high']\nids.block_n_plus_one_parent_hash_little.low = program_input['block_n_plus_one_parent_hash_little_low']\nids.block_n_plus_one_parent_hash_little.high = program_input['block_n_plus_one_parent_hash_little_high']";
-pub const HINT_INPUT_PREV: &str = "from tools.py.hints import write_uint256_array\nsegments.write_arg(ids.previous_peaks_values_poseidon, program_input['poseidon_mmr_last_peaks']) \nwrite_uint256_array(memory, ids.previous_peaks_values_keccak, program_input['keccak_mmr_last_peaks'])";
-pub const HINT_INPUT_BLOCK_HEADERS: &str = "block_headers_array = program_input['block_headers_array']\nbytes_len_array = program_input['bytes_len_array']\nsegments.write_arg(ids.block_headers_array, block_headers_array)\nsegments.write_arg(ids.block_headers_array_bytes_len, bytes_len_array)";
+pub const HINT_INPUT: &str = "ids.from_block_number_high=program_input['from_block_number_high']\nids.to_block_number_low=program_input['to_block_number_low']\nids.mmr_offset=program_input['mmr_last_len']\nids.mmr_last_root_poseidon=program_input['mmr_last_root_poseidon']\nids.block_n_plus_one_parent_hash = program_input['block_n_plus_one_parent_hash_little']";
+pub const HINT_INPUT_PREV: &str = "segments.write_arg(ids.previous_peaks_values_poseidon, program_input['poseidon_mmr_last_peaks'])";
+pub const HINT_INPUT_BLOCK_HEADERS: &str = "block_headers_array = program_input['block_headers_array']\nsegments.write_arg(ids.block_headers_array, block_headers_array)";
 
 impl CustomHintProcessor {
     pub fn hint_input(
@@ -33,20 +33,9 @@ impl CustomHintProcessor {
         let mmr_last_root_poseidon: Felt252 =
             serde_json::from_value(self.private_inputs["mmr_last_root_poseidon"].clone())
                 .unwrap_or_default();
-        let mmr_last_root_keccak_low: Felt252 =
-            serde_json::from_value(self.private_inputs["mmr_last_root_keccak_low"].clone())
+        let block_n_plus_one_parent_hash: Felt252 =
+            serde_json::from_value(self.private_inputs["block_n_plus_one_parent_hash"].clone())
                 .unwrap_or_default();
-        let mmr_last_root_keccak_high: Felt252 =
-            serde_json::from_value(self.private_inputs["mmr_last_root_keccak_high"].clone())
-                .unwrap_or_default();
-        let block_n_plus_one_parent_hash_little_low: Felt252 = serde_json::from_value(
-            self.private_inputs["block_n_plus_one_parent_hash_little_low"].clone(),
-        )
-        .unwrap_or_default();
-        let block_n_plus_one_parent_hash_little_high: Felt252 = serde_json::from_value(
-            self.private_inputs["block_n_plus_one_parent_hash_little_high"].clone(),
-        )
-        .unwrap_or_default();
 
         utils::write_value(
             "from_block_number_high",
@@ -73,29 +62,8 @@ impl CustomHintProcessor {
             hint_data,
         )?;
         utils::write_value(
-            "mmr_last_root_poseidon",
-            MaybeRelocatable::Int(mmr_last_root_poseidon),
-            vm,
-            hint_data,
-        )?;
-        utils::write_struct(
-            "mmr_last_root_keccak",
-            &[mmr_last_root_keccak_low, mmr_last_root_keccak_high]
-                .into_iter()
-                .map(MaybeRelocatable::Int)
-                .collect::<Vec<_>>(),
-            vm,
-            hint_data,
-        )?;
-        utils::write_struct(
-            "block_n_plus_one_parent_hash_little",
-            &[
-                block_n_plus_one_parent_hash_little_low,
-                block_n_plus_one_parent_hash_little_high,
-            ]
-            .into_iter()
-            .map(MaybeRelocatable::Int)
-            .collect::<Vec<_>>(),
+            "block_n_plus_one_parent_hash",
+            MaybeRelocatable::Int(block_n_plus_one_parent_hash),
             vm,
             hint_data,
         )?;
@@ -113,18 +81,8 @@ impl CustomHintProcessor {
         let poseidon_mmr_last_peaks: Vec<Felt252> =
             serde_json::from_value(self.private_inputs["poseidon_mmr_last_peaks"].clone()).unwrap();
 
-        let keccak_mmr_last_peaks: Vec<Vec<Felt252>> =
-            serde_json::from_value(self.private_inputs["keccak_mmr_last_peaks"].clone()).unwrap();
-
         let previous_peaks_values_poseidon_ptr = get_ptr_from_var_name(
             "previous_peaks_values_poseidon",
-            vm,
-            &hint_data.ids_data,
-            &hint_data.ap_tracking,
-        )?;
-
-        let previous_peaks_values_keccak_ptr = get_ptr_from_var_name(
-            "previous_peaks_values_keccak",
             vm,
             &hint_data.ids_data,
             &hint_data.ap_tracking,
@@ -138,10 +96,6 @@ impl CustomHintProcessor {
             vm.insert_value((previous_peaks_values_poseidon_ptr + j)?, &value)?;
         }
 
-        for (j, array) in keccak_mmr_last_peaks.into_iter().flatten().enumerate() {
-            vm.insert_value((previous_peaks_values_keccak_ptr + j)?, array)?;
-        }
-
         Ok(())
     }
 
@@ -153,20 +107,10 @@ impl CustomHintProcessor {
         _constants: &HashMap<String, Felt252>,
     ) -> Result<(), HintError> {
         let block_headers_array: Vec<Vec<Felt252>> =
-            serde_json::from_value(self.private_inputs["block_headers_array"].clone()).unwrap();
-
-        let bytes_len_array: Vec<Felt252> =
-            serde_json::from_value(self.private_inputs["bytes_len_array"].clone()).unwrap();
+            serde_json::from_value(self.private_inputs["preimages_array"].clone()).unwrap();
 
         let block_headers_array_ptr = get_ptr_from_var_name(
             "block_headers_array",
-            vm,
-            &hint_data.ids_data,
-            &hint_data.ap_tracking,
-        )?;
-
-        let block_headers_array_bytes_len_ptr = get_ptr_from_var_name(
-            "block_headers_array_bytes_len",
             vm,
             &hint_data.ids_data,
             &hint_data.ap_tracking,
@@ -178,14 +122,6 @@ impl CustomHintProcessor {
                 vm.insert_value((segment + k)?, &value)?;
             }
             vm.insert_value((block_headers_array_ptr + j)?, segment)?;
-        }
-
-        for (j, value) in bytes_len_array
-            .into_iter()
-            .map(MaybeRelocatable::Int)
-            .enumerate()
-        {
-            vm.insert_value((block_headers_array_bytes_len_ptr + j)?, &value)?;
         }
 
         Ok(())
